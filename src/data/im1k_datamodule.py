e0976c213d05d1ec3ffa6fbccb1549247987aeaf
Diego Canez <canezdiego@gmail.com>
2024-12-05 22:21:55 +0100

    feat: add im1k datamodule

---
+++ src/data/im1k_datamodule.py
@@ -0,0 +1,147 @@
+from typing import Any, Dict, Optional
+
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision.transforms import transforms
+from datasets import load_dataset
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class IM1KDataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ) -> None:
+        """Initialize a `MNISTDataModule`.
+
+        :param data_dir: The data directory. Defaults to `"data/"`.
+        :param batch_size: The batch size. Defaults to `64`.
+        :param num_workers: The number of workers. Defaults to `0`.
+        :param pin_memory: Whether to pin memory. Defaults to `False`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+        self.batch_size_per_device = batch_size
+        self.cli_logger = logging.getLogger(self.__class__.__name__)
+
+    @property
+    def num_classes(self) -> int:
+        """Get the number of classes.
+
+        :return: The number of MNIST classes (10).
+        """
+        return 10
+
+    def prepare_data(self) -> None:
+        """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
+        within a single process on CPU, so you can safely add your downloading logic within. In
+        case of multi-node training, the execution of this hook depends upon
+        `self.prepare_data_per_node()`.
+
+        Do not use it to assign state (self.x = y).
+        """
+        logging.info("Preparing Imagenet 1k")
+        logging.warning(
+            "Before running this, make sure you use the HF CLI to download the data:\n"
+            "huggingface-cli download ILSVRC/imagenet-1k --repo-type dataset"
+        )
+
+        # This call should just be used to decompress the data, not download the data.
+        _ = load_dataset("ILSVRC/imagenet-1k")
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = (
+                self.hparams.batch_size // self.trainer.world_size
+            )
+
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            self.data_train = load_dataset(
+                "ILSVRC/imagenet-1k", split="train"
+            ).with_format("torch")
+            self.data_val = load_dataset(
+                "ILSVRC/imagenet-1k", split="validation"
+            ).with_format("torch")
+            self.data_test = load_dataset(
+                "ILSVRC/imagenet-1k", split="test"
+            ).with_format("torch")
+
+            self.data_train.set_transform(self.transforms)
+            self.data_val.set_transform(self.transforms)
+            self.data_test.set_transform(self.transforms)
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+
+if __name__ == "__main__":
+    _ = IM1KDataModule()
