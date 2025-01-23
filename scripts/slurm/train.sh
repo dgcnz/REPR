aad7c984fa6d0cb6852045bc502e5a99740f9e5b
Diego Canez <canezdiego@gmail.com>
2025-01-23 13:39:08 +0100

    fix: slurm train

--- scripts/slurm/train.sh
+++ scripts/slurm/train.sh
@@ -2,14 +2,12 @@
 
 #SBATCH --partition=gpu_a100
 #SBATCH --nodes=1             
-#SBATCH --gpus=4
 #SBATCH --ntasks-per-node=4   
 #SBATCH --time=01:00:00
 #SBATCH --job-name=train
 #SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
 
 module load 2024
-# module load Python/3.11.3-GCCcore-12.3.0
 module load CUDA/12.6.0
 
 cd $HOME/development/PART
