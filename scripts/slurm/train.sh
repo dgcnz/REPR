667cc9352e88ebfb68d9d4cc5fbde74984af4cca
Diego Canez <canezdiego@gmail.com>
2025-01-23 13:02:38 +0100

    fix: ddp slurm script

--- scripts/slurm/train.sh
+++ scripts/slurm/train.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu
-#SBATCH --gpus=4
-#SBATCH --job-name=train
-#SBATCH --ntasks=1
+#SBATCH --partition=gpu_a100
+#SBATCH --nodes=1             
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=4   
 #SBATCH --time=01:00:00
+#SBATCH --job-name=train
 #SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
 
 module load 2024
@@ -14,7 +15,10 @@ module load CUDA/12.6.0
 cd $HOME/development/PART
 source .venv/bin/activate
 
-╰─❯ python -m src.train hparams_search=im1k_A100 experiment=hs_part_im1k_pairdiff_mlp_A100
+export NCCL_DEBUG=INFO
+export PYTHONFAULTHANDLER=1
+
+srun python -m src.train hparams_search=im1k_A100 experiment=hs_part_im1k_pairdiff_mlp_A100 trainer.num_nodes=1 data.batch_size=256 model.compile=False
 
 deactivate
 module unload CUDA/12.6.0
