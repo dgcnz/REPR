86938a31f3d5e4fe6510e9a358e0a52b2fcc2c49
Diego Canez <canezdiego@gmail.com>
2025-01-26 11:53:21 +0100

    fix: only hook on train epochs

--- src/callbacks/loggers/head_hook.py
+++ src/callbacks/loggers/head_hook.py
@@ -13,31 +13,37 @@ class HeadHookLogger(BaseCallback):
         self.head_inputs = []
         self.head_outputs = []
         self.every_n_steps = every_n_steps
-        self.handle = None
+        self.handles = []
 
-    def _hook_fn(self, module, input, output):
+    def _head_hook_fn(self, module, input, output):
         self.head_input_features = clean_tensor(input[0])
         # self.head_input_patch_pair_indices = clean_tensor(input[1])
         self.head_outputs = clean_tensor(output)
+    
+    def _model_hook_fn(self, module, input, output):
+        # rescale by logit_scale to [-1, 1]
+        self.model_output = clean_tensor(output) / module.logit_scale
 
     @rank_zero_only
-    def on_stage_batch_start(self, trainer, pl_module, batch, batch_idx, stage: str):
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
         # Only register the hook
         if should_log(batch_idx, self.every_n_steps):
-            self.handle = pl_module.net.head.register_forward_hook(self._hook_fn)
+            self.handles =[
+                pl_module.net.head.register_forward_hook(self._head_hook_fn),
+                pl_module.net.register_forward_hook(self._model_hook_fn)
+            ]
 
     @rank_zero_only
-    def on_stage_batch_end(
-        self, trainer, pl_module, outputs, batch, batch_idx, stage: str
-    ):
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         if not should_log(batch_idx, self.every_n_steps):
             return
-
-        assert self.handle is not None
-        self.handle.remove()
-        self.handle = None
+        
+        assert self.handles
+        for handle in self.handles:
+            handle.remove()
+        self.handles = []
         # Create figure 1: Head Input Features histogram
-        fig, axes = plt.subplots(3, 1, figsize=(10, 15))
+        fig, axes = plt.subplots(4, 1, figsize=(10, 20))
         axes[0].hist(self.head_input_features.flatten(), bins=50)
         axes[0].set_title("Head Input Features")
         
@@ -49,10 +55,13 @@ class HeadHookLogger(BaseCallback):
         if isinstance(pl_module.net.head, PairDiffMLP):
             axes[2].hist(clean_tensor(pl_module.net.head.proj.weight).flatten(), bins=50)
             axes[2].set_title("PairDiff Weights")
-    
+
+        # Create figure 4: Model Output histogram
+        axes[3].hist(self.model_output.flatten(), bins=50)
+        axes[3].set_title("Model Output")
 
         self.log_plots(
             {
-                f"{stage}/head_hook": fig,
+                f"train/head_hook": fig,
             }
         )
