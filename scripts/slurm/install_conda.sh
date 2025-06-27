4c330279a67255f3dcb680b3f1aada51f172316d
Diego Canez <canezdiego@gmail.com>
2025-06-27 12:03:33 +0200

    feat: conda

---
+++ scripts/slurm/install_conda.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_mig
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --time=01:00:00
+#SBATCH --job-name=train
+#SBATCH --output=scripts/slurm_logs/%A.out
+
+module load 2024
+module load CUDA/12.6.0
+module load Anaconda3
+
+cd $HOME/development/PART
+
+export PYTHONFAULTHANDLER=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export HF_HOME="/scratch-shared/dcanez/HF_HOME"
+export TORCHINDUCTOR_CACHE_DIR="/scratch-local/dcanez/tmp/torchinductor/"
+export HYDRA_FULL_ERROR=1 
+
+srun conda env create -f env.yaml
+
+module unload Anaconda3
+module unload CUDA/12.6.0
+module unload 2024
