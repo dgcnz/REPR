f3abc743c8b2ff4fe60adf840353a39da38501fa
Diego Canez <canezdiego@gmail.com>
2025-07-08 13:03:18 +0200

    fmt: cleanup

--- src/callbacks/loggers/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.callbacks.loggers.head_hook import HeadHookLogger
-from src.callbacks.loggers.reconstruction import ReconstructionLogger
-from src.callbacks.loggers.transform import TransformLogger
-from src.callbacks.loggers.metrics import MetricLogger
